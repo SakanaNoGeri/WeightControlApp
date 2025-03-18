9a54d83181054104d18c84e4d22e70bb39ad3a8e
DenisHome <d.shapovalov@tg.dunice.net>
2025-03-18 06:35:20 +0300

    initial commit

---
+++ src/main/java/com/example/weightcontrolapp/WeightControlAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.weightcontrolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightControlAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightControlAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weightcontrolapp/WeightControlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weightcontrolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightControlAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
